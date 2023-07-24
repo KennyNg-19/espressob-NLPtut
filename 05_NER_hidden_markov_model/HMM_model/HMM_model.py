94b033c7f69b79138e6a4df3152b0e6c4208289b
YuhaoWU <wuyuhao2019@126.com>
2023-07-25 02:31:15 +0800

    init跑通

--- 05_NER_hidden_markov_model/HMM_model/HMM_model.py
+++ 05_NER_hidden_markov_model/HMM_model/HMM_model.py
@@ -6,20 +6,19 @@ from tqdm import tqdm
 class HMM_NER:
     def __init__(self, char2idx_path, tag2idx_path):
         # 载入一些字典
-        # char2idx: 字 转换为 token
+        # char2idx: 字 转换为 token idx
         self.char2idx = load_dict(char2idx_path)
-        # tag2idx: 标签转换为 token
+        # tag2idx: 标签转换为 token idx
         self.tag2idx = load_dict(tag2idx_path)
-        # idx2tag: token转换为标签
+        # idx2tag: token idx转换为标签
         self.idx2tag = {v: k for k, v in self.tag2idx.items()}
         # 初始化隐状态数量(实体标签数)和观测数量(字数)
         self.tag_size = len(self.tag2idx)
         self.vocab_size = max([v for _, v in self.char2idx.items()]) + 1
-        # 初始化A, B, pi为全0
-        self.transition = np.zeros([self.tag_size,
-                                    self.tag_size])
-        self.emission = np.zeros([self.tag_size,
-                                  self.vocab_size])
+
+        # 初始化矩阵A, B, pi为全0
+        self.transition = np.zeros([self.tag_size, self.tag_size])
+        self.emission = np.zeros([self.tag_size, self.vocab_size])
         self.pi = np.zeros(self.tag_size)
         # 偏置, 用来防止log(0)或乘0的情况
         self.epsilon = 1e-8
@@ -34,30 +33,14 @@ class HMM_NER:
         # 估计转移概率矩阵, 发射概率矩阵和初始概率矩阵的参数
         self.estimate_transition_and_initial_probs(train_dic)
         self.estimate_emission_probs(train_dic)
-        # take the logarithm
-        # 取log防止计算结果下溢
+
+        # take the logarithm，乘法 -> 加法，取log【防止概率过小的数，计算结果下溢】
         self.pi = np.log(self.pi)
         self.transition = np.log(self.transition)
         self.emission = np.log(self.emission)
         print("DONE!")
 
-
-    def estimate_emission_probs(self, train_dic):
-        """
-        发射矩阵参数的估计
-        estimate p( Observation | Hidden_state )
-        :param train_dic:
-        :return:
-        """
-        print("estimating emission probabilities...")
-        for dic in tqdm(train_dic):
-            for char, tag in zip(dic["text"], dic["label"]):
-                self.emission[self.tag2idx[tag],
-                              self.char2idx[char]] += 1
-        self.emission[self.emission == 0] = self.epsilon
-        self.emission /= np.sum(self.emission, axis=1, keepdims=True)
-
-
+    # 发射矩阵A 和 π
     def estimate_transition_and_initial_probs(self, train_dic):
         """
         转移矩阵和初始概率的参数估计, 也就是bigram二元模型
@@ -68,22 +51,38 @@ class HMM_NER:
         print("estimating transition and initial probabilities...")
         for dic in tqdm(train_dic):
             for i, tag in enumerate(dic["label"][:-1]):
+                # pi 需要的 i = 0时
                 if i == 0:
                     self.pi[self.tag2idx[tag]] += 1
                 curr_tag = self.tag2idx[tag]
-                next_tag = self.tag2idx[dic["label"][i+1]]
+                next_tag = self.tag2idx[dic["label"][i + 1]]  # 找到相邻两个标签
                 self.transition[curr_tag, next_tag] += 1
         self.transition[self.transition == 0] = self.epsilon
         self.transition /= np.sum(self.transition, axis=1, keepdims=True)
         self.pi[self.pi == 0] = self.epsilon
         self.pi /= np.sum(self.pi)
 
+    # 发射矩阵B
+    def estimate_emission_probs(self, train_dic):
+        """
+        发射矩阵B参数的估计
+        estimate p( Observation | Hidden_state )
+        :param train_dic:
+        :return:
+        """
+        print("estimating emission probabilities...")
+        for dic in tqdm(train_dic):
+            for char, tag in zip(dic["text"], dic["label"]):
+                self.emission[self.tag2idx[tag], self.char2idx[char]] += 1
+        self.emission[self.emission == 0] = self.epsilon  # 防止log 0,
+        self.emission /= np.sum(self.emission, axis=1, keepdims=True)
+
     def get_p_Obs_State(self, char):
         # 计算p( observation | state)
         # 如果当前字属于未知, 则讲p( observation | state)设为均匀分布
         char_token = self.char2idx.get(char, 0)
         if char_token == 0:
-            return np.log(np.ones(self.tag_size)/self.tag_size)
+            return np.log(np.ones(self.tag_size) / self.tag_size)
         return np.ravel(self.emission[:, char_token])
 
     def predict(self, text):
@@ -97,7 +96,7 @@ class HMM_NER:
     def print_func(self, text, best_tags_id):
         # 用来打印预测结果
         for char, tag_id in zip(text, best_tags_id):
-            print(char+"_"+self.idx2tag[tag_id]+"|", end="")
+            print(char + "_" + self.idx2tag[tag_id] + "|", end="")
 
     def viterbi_decode(self, text):
         """
@@ -122,7 +121,7 @@ class HMM_NER:
             # 这里用到了矩阵化计算方法, 详见视频教程
             p_Obs_State = self.get_p_Obs_State(text[i])
             p_Obs_State = np.expand_dims(p_Obs_State, axis=0)
-            prev_score = np.expand_dims(T1_table[i-1, :], axis=-1)
+            prev_score = np.expand_dims(T1_table[i - 1, :], axis=-1)
             # 广播算法, 发射概率和转移概率广播 + 转移概率
             curr_score = prev_score + self.transition + p_Obs_State
             # 存入T1 T2中
@@ -130,14 +129,18 @@ class HMM_NER:
             T2_table[i, :] = np.argmax(curr_score, axis=0)
         # 回溯
         best_tag_id = int(np.argmax(T1_table[-1, :]))
-        best_tags = [best_tag_id, ]
-        for i in range(seq_len-1, 0, -1):
+        best_tags = [
+            best_tag_id,
+        ]
+        for i in range(seq_len - 1, 0, -1):
             best_tag_id = int(T2_table[i, best_tag_id])
             best_tags.append(best_tag_id)
         return list(reversed(best_tags))
 
+
 if __name__ == '__main__':
-    model = HMM_NER(char2idx_path="./dicts/char2idx.json",
-                    tag2idx_path="./dicts/tag2idx.json")
+    model = HMM_NER(
+        char2idx_path="./dicts/char2idx.json", tag2idx_path="./dicts/tag2idx.json"
+    )
     model.fit("./corpus/train_data.txt")
     model.predict("我在中国吃美国的面包")
